281fcaa559c320b8ef768f978489cc13b282e984
José David Jiménez Badilla <jose51949@gmail.com>
2021-03-10 15:55:52 -0600

    Initial commit

---
+++ docs/precache-manifest.b3eb30ae6266c3bb66c78cdf6784eb12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3e334de5f21e3876fa808e1c4ab8f88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8168783013e7489482c9",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8168783013e7489482c9",
+    "url": "/static/js/main.e8218fd5.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
